948c75770431c35e5ea8b07aa7ed1c0f605d55f0
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-07 15:18:05 +0100

    fixed the bugs of the beginning

--- src/containers/cars_index.js
+++ src/containers/cars_index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class CarsIndex extends Component {
+  componentDidMount() {
+    console.log("hello from the cars index component")
+  }
+  
   render() {
     return (
       <div><h1>Hello from cars index</h1></div>
--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { createHistory as history } from 'history';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserHistory as history } from 'history';
 
-import '../assets/stylesheets/application.scss';
+import './assets/stylesheets/application.css';
 
 // components and containers
 import CarsIndex from './containers/cars_index';
 
 // reducers
 import carsReducer from './reducers/cars_reducer';
+import garageReducer from './reducers/garage_reducer';
 
 // const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
 const initialState = {
   garage: "myGarage",
-  cars: [
-    { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
-    { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
-    { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
-    { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
-  ]
+  cars: []
 };
 
 const reducers = combineReducers({
-  garage: (state = null, action) => state,
+  garage: garageReducer, // (state = null, action) => state,
   cars: carsReducer
 });
 
@@ -35,18 +31,29 @@ const middlewares = applyMiddleware(reduxPromise, logger);
 const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={CarsIndex} />
-      </Switch>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={history}>
+//       <Routes>
+//         <Route path="/" exact component={CarsIndex} />
+//       </Routes>
+//     </Router>
+//   </Provider>,
+//   document.querySelector('.container')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes>
+          <Route path="/" exact component={CarsIndex} />
+        </Routes>
+      </Router>
+    </Provider>
+  // </React.StrictMode>
 );
-
-
 
 // //////////////////////////////////////////////////////////////////////////////
 
